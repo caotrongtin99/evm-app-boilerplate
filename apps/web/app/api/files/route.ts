0fed8a955c7351d52627f6834d7d7f93343a646b
caotrongtin99 <tin.caotrong@gmail.com>
2025-06-28 11:56:29 +0700

    fix: add file validation

--- apps/web/app/api/files/route.ts
+++ apps/web/app/api/files/route.ts
@@ -5,8 +5,17 @@ export async function POST(request: NextRequest) {
   try {
     const data = await request.formData();
     const file: File | null = data.get("file") as unknown as File;
+    const MAX_FILE_SIZE = 10 * 1024 * 1024;
+
+    const validateFile = (file: File) => {
+      if (file.size > MAX_FILE_SIZE) {
+        throw new Error("File too large");
+      }
+    };
+
+    validateFile(file);
+
     const { cid } = await pinata.upload.public.file(file);
-    console.log({ pinata });
     const url = await pinata.gateways.public.convert(cid);
     return NextResponse.json(url, { status: 200 });
   } catch (e) {
